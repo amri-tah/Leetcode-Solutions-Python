9a5a536f7af85532feea00ec88bac5ce2a6067ac
Amritha Nandini <111682039+amri-tah@users.noreply.github.com>
2024-02-23 19:06:09 +0530

    Time: 37 ms (52.06%), Space: 16.6 MB (86.41%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -5,7 +5,7 @@ class Solution:
         for letter in s:
             if letter in temp:
                 stack.append(letter)
-            elif letter in [")", "}", "]"]:
+            elif letter in temp.values():
                 if len(stack)==0:
                     return False
                 elif letter==temp[stack.pop()]:
