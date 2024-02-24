d7cb8e4cf934c413fcd03efe69d9ddda6ae29ed8
Amritha Nandini <111682039+amri-tah@users.noreply.github.com>
2024-02-24 11:23:42 +0530

    Time: 127 ms (13.04%), Space: 17.5 MB (59.14%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,17 +3,15 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rows_i, cols_i = self.findZeroes(matrix)
+        rows_i, cols_i = [], []
+        
+        for row in range(len(matrix)):
+            for col in range(len(matrix[row])):
+                if matrix[row][col] == 0:
+                    rows_i.append(row)
+                    cols_i.append(col)
+        
         for row in range(len(matrix)):
             for col in range(len(matrix[row])):
                 if row in rows_i or col in cols_i:
                     matrix[row][col] = 0
-        
-    def findZeroes(self, matrix):
-        rows, cols = [], []
-        for row in range(len(matrix)):
-            for col in range(len(matrix[row])):
-                if matrix[row][col] == 0:
-                    rows.append(row)
-                    cols.append(col)
-        return rows, cols
