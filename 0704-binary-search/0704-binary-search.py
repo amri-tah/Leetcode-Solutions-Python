fc9386d45837d7d061cebc36e12b4aa643bde27c
Amritha Nandini <111682039+amri-tah@users.noreply.github.com>
2024-02-27 17:06:47 +0530

    Time: 188 ms (80.73%), Space: 18.2 MB (47.31%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,7 +1,5 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # if len(nums)==1: return 0
-        # Set 2 pointers
         left, right = 0, len(nums)-1
         
         while left<=right:
