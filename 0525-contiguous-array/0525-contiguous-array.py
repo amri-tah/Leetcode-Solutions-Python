bcc0911c0735759311497ba9a4a539105a7f423f
Amritha Nandini <111682039+amri-tah@users.noreply.github.com>
2024-03-16 21:33:55 +0530

    Time: 608 ms (83.65%), Space: 22.2 MB (70.11%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -3,10 +3,7 @@ class Solution:
         hash = {0: -1}
         maxlen, count = 0, 0
         for i in range(len(nums)):
-            
             count += 1 if (nums[i] == 1) else -1
-            if count in hash:
-                maxlen = max(maxlen, i-hash[count])
-            else:
-                hash[count]=i
+            if count in hash: maxlen = max(maxlen, i-hash[count])
+            else: hash[count]=i
         return maxlen;
