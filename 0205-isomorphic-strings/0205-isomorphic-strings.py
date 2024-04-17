5c0a54b8b61394d0afaa55f6723e2da89caefb75
Amritha Nandini <111682039+amri-tah@users.noreply.github.com>
2024-04-17 18:21:11 +0530

    Time: 43 ms (50.49%), Space: 17.1 MB (16.33%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s)!= len(t): return False
+        
+        shash = {}
+        stable = []
+        i = 0
+        for s_char in s:
+            if s_char in shash:
+                stable.append(shash[s_char])
+            else:
+                shash[s_char] = i
+                stable.append(i)
+                i+=1
+
+        thash = {}
+        i = 0
+        for j, t_char in enumerate(t):
+            if t_char not in thash:
+                thash[t_char] = i
+                i+=1
+            if thash[t_char]!=stable[j]: return False
+        
+        
+        return True
