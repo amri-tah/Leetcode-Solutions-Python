a2075c7d1570067e302c46975eed4e4efcc66c6d
Amritha Nandini <111682039+amri-tah@users.noreply.github.com>
2024-06-17 21:00:37 +0530

    Time: 38 ms (38.60%), Space: 16.5 MB (91.60%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,9 +4,10 @@ class Solution:
         stack = []
         for letter in s:
             if letter in brackets: stack.append(letter)
-            elif stack:
-                popped = stack.pop()
-                if letter!=brackets[popped]: return False
-            else: return False
-        if stack: return False
-        return True
+            else:
+                if stack:
+                    popped = stack.pop()
+                    if letter!=brackets[popped]: return False
+                else: return False
+                
+        return len(stack)==0
