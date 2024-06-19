d730e1339cf67230c8e2470b8e7cb255d3f55f70
Amritha Nandini <111682039+amri-tah@users.noreply.github.com>
2024-06-19 14:54:11 +0530

    Time: 39 ms (45.82%), Space: 16.4 MB (61.81%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,4 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        freq = defaultdict(int)
+        for num in nums:
+            freq[num]+=1
+        for i in range(len(nums)):
+            if i<freq[0]: nums[i]=0
+            elif i>=freq[0] and i<freq[0]+freq[1]: nums[i]=1
+            else: nums[i]=2
+                
