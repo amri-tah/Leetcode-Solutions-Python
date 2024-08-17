6312cb82084b6c3fe6c248eabcf25a58e4496f36
Amritha Nandini <111682039+amri-tah@users.noreply.github.com>
2024-08-17 17:16:15 +0530

    Time: 744 ms (75.4%), Space: 17 MB (38.8%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,11 +1,11 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        # How many coins to get to denomination i
         dp = [amount+1]*(amount+1)
-        dp[0] = 0
+        dp[0] = 0 # No of coins to get amt 0 = 0
         
         for i in range(1, amount+1):
-            for den in coins:
-                if i>=den: dp[i] = min(dp[i], dp[i-den]+1)
+            for d in coins:
+                if i-d>=0: dp[i] = min(dp[i], 1+dp[i-d])
                 
-        if dp[amount]!= amount+1: return dp[amount]
-        else: return -1
+        return dp[amount] if dp[-1]!=amount+1 else -1
