bfaf1289b70113281e9c91c4caebe52e39580828
Amritha Nandini <111682039+amri-tah@users.noreply.github.com>
2024-08-18 15:35:28 +0530

    Time: 98 ms (16.24%), Space: 22.1 MB (13.63%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,17 +1,11 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        d = {}
-        for num in nums:
-            if num in d:
-                d[num]+=1
-            else:
-                d[num]=0
-        freq = [key for key, value in sorted(d.items(), key = lambda x: x[1], reverse=True)]
-        return freq[0:k]
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = Counter(nums)
+        result, bucket = [], [[] for i in range(len(nums)+1)]
 
-
+        for key, val in freq.items():
+            bucket[val].append(key)
+        for i in range(len(nums), 0, -1):
+            for n in bucket[i]: 
+                result.append(n)
+                if len(result)==k: return result
