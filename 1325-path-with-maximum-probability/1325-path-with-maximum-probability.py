9ca192d97fbd56bfdd7a7b1f9de79a85100902b9
Amritha Nandini <111682039+amri-tah@users.noreply.github.com>
2024-08-27 08:04:02 +0530

    Time: 692 ms (6.28%), Space: 28.6 MB (55.32%) - LeetHub

---
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        graph = defaultdict(list)
+        for i in range(len(edges)):
+            u, v = edges[i]
+            graph[u].append([v, succProb[i]])
+            graph[v].append([u, succProb[i]])
+        
+        visited = set()
+        probability = [0]*n
+        max_heap = []
+        for neigh, prob in graph[start_node]:
+            heapq.heappush(max_heap, (-prob, neigh))
+        visited.add(start_node)
+        probability[start_node]=1
+
+        while max_heap:
+            prob, node = heapq.heappop(max_heap)
+            visited.add(node)
+            probability[node] = max(probability[node], -prob)
+            for neigh, next_prob in graph[node]:
+                if neigh not in visited: heapq.heappush(max_heap, (prob*next_prob, neigh))
+        return probability[end_node]
