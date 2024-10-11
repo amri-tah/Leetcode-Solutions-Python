5e42cd6c58112aeab92a139e88c6f98d44961ca0
Amritha Nandini <111682039+amri-tah@users.noreply.github.com>
2024-10-11 16:11:33 +0530

    Time: 232 ms (78.97%), Space: 18.9 MB (85.65%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,14 +5,15 @@ class Solution:
         def bfs(r, c):
             queue = deque([(r,c)])
             directions = [(0,1), (1,0), (-1,0), (0,-1)]
+            grid[r][c]="2"
             while queue:
                 r, c = queue.popleft()
-                grid[r][c]="2"
                 for x, y in directions:
                     nx, ny = r+x, c+y
                     if 0<=nx<m and 0<=ny<n and grid[nx][ny]=="1":
                         queue.append((nx, ny))
-                
+                        grid[nx][ny]="2"
+
 
         for r in range(m):
             for c in range(n):
