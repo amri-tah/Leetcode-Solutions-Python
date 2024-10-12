50a0e197793044a866052e3b8bfdee76037714c0
Amritha Nandini <111682039+amri-tah@users.noreply.github.com>
2024-10-12 10:35:05 +0530

    Time: 259 ms (91.35%), Space: 16.6 MB (45.83%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,39 +1,23 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # if s==s[::-1]: return s
-        
-        # def pallindrome(s):
-        #     return s==s[::-1]
-        
-        # # Brute Force Approach: using 2 pointer for indices and checking if pallindrome
-        # maxP = s[0]
-        # for left in range(len(s)):
-        #     for right in range(left+1, len(s)):
-        #         if pallindrome(s[left:right+1]) and right-left+1>len(maxP): 
-        #             maxP = s[left:right+1]
-                    
-        # return maxP
-
-        # DP Approach: consider every character as center and expand outwards
-        result = ""
-        max_len = 0
-
+        maxLen = 0
+        output = ""
         for i in range(len(s)):
-            # Odd Length
-            l, r = i, i
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                if (r-l+1)>max_len:
-                    result = s[l:r+1]
-                    max_len = r-l+1
-                l-=1
-                r+=1
-                
-            # Even Length
-            l, r = i, i+1
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                if (r-l+1)>max_len:
-                    result = s[l:r+1]
-                    max_len = r-l+1
-                l-=1
-                r+=1
-        return result
+            # odd len
+            left, right = i, i
+            while left>=0 and right<len(s) and s[left]==s[right]:
+                left-=1
+                right+=1
+            if right-left-1>maxLen: 
+                maxLen = right-left-1
+                output = s[left+1:right]
+            
+            # even len
+            left, right = i, i+1
+            while left>=0 and right<len(s) and s[left]==s[right]:
+                left-=1
+                right+=1
+            if right-left-1>maxLen: 
+                maxLen = right-left-1
+                output = s[left+1:right]
+        return output
