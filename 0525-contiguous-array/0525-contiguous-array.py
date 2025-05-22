f436acfecede11caf70647f572eed0cad0aa2922
Amritha Nandini K L <111682039+amri-tah@users.noreply.github.com>
2025-05-22 07:53:52 +0530

    Time: 99 ms (74.04%), Space: 23.5 MB (49.73%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,9 +1,14 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        hash = {0: -1}
-        maxlen, count = 0, 0
-        for i in range(len(nums)):
-            count += 1 if (nums[i] == 1) else -1
-            if count in hash: maxlen = max(maxlen, i-hash[count])
-            else: hash[count]=i
-        return maxlen;
+        maxL = prefixSum = 0
+        hash = {0: -1} # sum: earliest index
+        # if 1 encountered => +1
+        # if 0 encountered => -1
+        # [-1, 0]
+        # [-1, 0, -1]
+        # [-1, 0, 1, 2, 3, 2, 1, 0]
+        for i, num in enumerate(nums):
+            prefixSum+=1 if num==1 else -1
+            if prefixSum in hash: maxL = max(maxL, i-hash[prefixSum])
+            else: hash[prefixSum]= i
+        return maxL
