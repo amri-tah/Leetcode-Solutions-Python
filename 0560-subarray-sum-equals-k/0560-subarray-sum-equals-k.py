e2962967b8bf9dcfb95a23baad42df8cc8dc7a53
Amritha Nandini K L <111682039+amri-tah@users.noreply.github.com>
2025-05-22 18:50:34 +0530

    Time: 23 ms (96.33%), Space: 20.4 MB (39.11%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,9 +1,9 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prefixSum  = {0: 1}
+        prefixSum  = {0: 1} # {sum: no of times the sum has occured}
         curr = count = 0
         for i in range(len(nums)):
             curr += nums[i]
-            count += prefixSum.get(curr-k, 0)
+            if curr-k in prefixSum: count+=prefixSum[curr-k]
             prefixSum[curr] = prefixSum.get(curr, 0)+1
         return count
