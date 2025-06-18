8efc7eddb84da743ed014ac7efb63e5615292db5
Amritha Nandini K L <111682039+amri-tah@users.noreply.github.com>
2025-06-18 20:41:01 +0530

    Time: 0 ms (100%), Space: 17.8 MB (34.61%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,26 +1,16 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         stack = []
-        
-        # Iterate thru each char in the string and push if not a closing bracket:
+        output = ""
         for char in s:
             if char!="]": stack.append(char)
             else:
-                # Pop out characters in between brackets
                 substr = ""
-                while stack[-1]!="[":
-                    substr = stack.pop() + substr
-                
-                stack.pop() # Pop out opening bracket
-                
-                # Pop out digits
-                subnumber = ""
+                while stack and stack[-1]!="[":
+                    substr=stack.pop()+substr
+                stack.pop()
+                num = ""
                 while stack and stack[-1].isdigit():
-                    subnumber = stack.pop() + subnumber
-                
-                subnumber = int(subnumber)
-                
-                # Push the decoded substring
-                stack.append(subnumber*substr)
-        
+                    num=stack.pop()+num
+                stack.append(substr*int(num))
         return "".join(stack)
