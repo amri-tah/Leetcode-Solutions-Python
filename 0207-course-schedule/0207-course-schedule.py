20fbc54922de0b512d8fe5366f6cf3cc94324450
Amritha Nandini K L <111682039+amri-tah@users.noreply.github.com>
2025-08-10 14:36:41 +0530

    Time: 3 ms (87.95%), Space: 19.5 MB (31.18%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,22 +1,18 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj = [[] for _ in range(numCourses)]
-        for prereq in prerequisites:
-            adj[prereq[1]].append(prereq[0])
-        
+        adj = {i:[] for i in range(numCourses)}
+        for u, v in prerequisites:
+            adj[u].append(v)
         visited = set()
-        def dfs(node):
-            if node in visited: return False # Loop in the graph
-            if len(adj[node])==0: return True # Finish-able task
-
-            visited.add(node)
-            for neigh in adj[node]:
-                if not dfs(neigh): return False
-                    
-            visited.remove(node)
-            adj[node] = []
+        def dfs(course):
+            if course in visited: return False
+            if not adj[course]: return True
+            visited.add(course)
+            for neigh in adj[course]:
+                if not dfs(neigh):return False
+            visited.remove(course)
+            adj[course]=[]
             return True
-
         for course in range(numCourses):
             if not dfs(course): return False
         return True
