3126d39397692f269d534ac3dc7f29eb45b6a6cb
Amritha Nandini K L <111682039+amri-tah@users.noreply.github.com>
2025-08-10 14:05:28 +0530

    Time: 14 ms (90.24%), Space: 18.2 MB (65.03%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -3,7 +3,7 @@ class Solution:
         m, n = len(grid), len(grid[0])
         maxarea = 0
         def dfs(r, c):
-            if r<0 or r>=m or c<0 or r>=n or grid[r][c]!=1: return 0
+            if r<0 or r>=m or c<0 or c>=n or grid[r][c]!=1: return 0
             grid[r][c]=2
             area = 1
             area += dfs(r, c+1)
