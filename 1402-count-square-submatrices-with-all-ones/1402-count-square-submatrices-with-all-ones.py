71a582d8460f9d753476c21f337ebdaffdd16586
Amritha Nandini K L <111682039+amri-tah@users.noreply.github.com>
2025-08-20 09:40:11 +0530

    Time: 162 ms (16.82%), Space: 20.1 MB (98.21%) - LeetHub

---
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        m, n = len(matrix), len(matrix[0])
+        def bfs(i, j):
+            size = 1
+            count = 0
+            while i + size <= m and j + size <= n:
+                for x in range(i, i + size):
+                    for y in range(j, j + size):
+                        if matrix[x][y] == 0: return count   
+                count += 1
+                size += 1
+            return count
+
+        total =0 
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if matrix[row][col]==1:
+                    total+=bfs(row, col)
+        return total
