41a0849fc52f539e09111531c2d60c49499014bb
Amritha Nandini K L <111682039+amri-tah@users.noreply.github.com>
2025-08-20 09:41:25 +0530

    Time: 174 ms (15.71%), Space: 20.2 MB (90.78%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
@@ -1,7 +1,9 @@
 class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
         m, n = len(matrix), len(matrix[0])
+        cache = {}
         def bfs(i, j):
+            if (i, j) in cache: return cache[(i,j)]
             size = 1
             count = 0
             while i + size <= m and j + size <= n:
@@ -10,6 +12,7 @@ class Solution:
                         if matrix[x][y] == 0: return count   
                 count += 1
                 size += 1
+            cache[(i, j)] = count
             return count
 
         total =0 
