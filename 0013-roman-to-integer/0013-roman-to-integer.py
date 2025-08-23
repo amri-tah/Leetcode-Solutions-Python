d8a901948c82314d41a64c4d4e62e4e36ca1c206
Amritha Nandini K L <111682039+amri-tah@users.noreply.github.com>
2025-08-23 10:31:43 +0530

    Time: 7 ms (39.89%), Space: 17.7 MB (90.22%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,11 +1,8 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        sum = 0
-        map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        for i in range(len(s) - 1):
-            if map[s[i]] < map[s[i + 1]]:
-                sum -= map[s[i]]
-            else:
-                sum += map[s[i]]
-        sum += map[s[-1]]
-        return sum
+        hash = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        res = 0
+        for i in range(0, len(s)-1):
+            if hash[s[i]]>=hash[s[i+1]]: res += hash[s[i]]
+            else:  res -= hash[s[i]]
+        return res + hash[s[-1]]
