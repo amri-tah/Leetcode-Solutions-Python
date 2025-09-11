45b3f6e2321ca6b17e3e7a52283b768078cafd61
Amritha Nandini K L <111682039+amri-tah@users.noreply.github.com>
2025-09-11 20:46:04 +0530

    Time: 80 ms (39.04%), Space: 33.6 MB (81.53%) - LeetHub

---
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        # player[i]<=trainer[j]
+        players.sort()
+        trainers.sort()
+        i, j = 0, 0
+        count = 0
+        while i<len(players) and j<len(trainers):
+            if players[i]<=trainers[j]:
+                i+=1
+                j+=1
+                count +=1
+            else: j+=1
+        return count
