549267d616f2de4cec69a2634983233e4cde2987
Amritha Nandini K L <111682039+amri-tah@users.noreply.github.com>
2025-09-12 12:47:36 +0530

    Time: 70 ms (12.85%), Space: 18 MB (14.61%) - LeetHub

---
+++ 1674-minimum-operations-to-make-array-equal/1674-minimum-operations-to-make-array-equal.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minOperations(self, n: int) -> int:
+        # req value => sum/n
+        # -=1 in the right side and +=1 in the left
+        req = sum([(2*i)+1 for i in range(n)])//n
+        ops = 0
+        left, right = 1, 2*(n-1)+1
+        while left<=right and left!=req and right!=req:
+            ops+=req-left
+            left+=2
+            right-=2
+        return ops
