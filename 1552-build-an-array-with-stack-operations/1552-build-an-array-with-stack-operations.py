8e2e7b7aeb475b5d0bdc71fc096012843e373b26
Amritha Nandini K L <111682039+amri-tah@users.noreply.github.com>
2025-11-18 13:29:17 +0530

    Time: 0 ms (100%), Space: 17.9 MB (13.08%) - LeetHub

--- 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.py
+++ 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.py
@@ -5,10 +5,10 @@ class Solution:
         top = 0
         for i in range(1, n+1):
             if stack==target: return res
-            stack.append(i)
-            res.append("Push")
-            if target[top]!=stack[-1]:
-                stack.pop()
-                res.append("Pop")
-            else: top+=1
+            if target[top]!=i:
+                res.extend(["Push","Pop"])
+            else: 
+                stack.append(i)
+                res.append("Push")
+                top+=1
         return res
